43570e1ce876f78e4e8b2e1aa4750e0380014d9a
Dominikus Baur <dominikus.baur@gmail.com>
2012-09-20 16:46:11 +0200

    added event handler for dropbox

--- visitsWeb/js/fileReader.js
+++ visitsWeb/js/fileReader.js
@@ -1,2 +1,30 @@
 // reads a file via HTML5
+// code is mostly taken from http://www.thebuzzmedia.com/html5-drag-and-drop-and-file-api-tutorial/
 
+var dropbox = document.getElementById("dropbox")
+ 
+// init event handlers
+dropbox.addEventListener("dragenter", dragEnter, false);
+dropbox.addEventListener("dragexit", dragExit, false);
+dropbox.addEventListener("dragover", dragOver, false);
+dropbox.addEventListener("drop", drop, false);
+
+function dragEnter(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+}
+
+function dragExit(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+}
+
+function dragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+}
+
+function drop(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+}
