f3490b60d29835e5ada9c9a7829207b8dddf8e1d
Dominikus Baur <dominikus.baur@gmail.com>
2012-09-20 17:17:55 +0200

    objectified fileReader renamed fileReader to dragAndDropFileReader

--- visitsWeb/js/maps.js
+++ visitsWeb/js/maps.js
@@ -1,16 +1,24 @@
-function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          noClear: true,
-          zoomControl: false,
-          panControl: false,
-          rotateControl: false,
-          scaleControl: false,
-          disableDefaultUI: true
+var handleReaderLoad = function(evt){
+	var extLocationHistory = JSON.parse(evt.target.result);
+	alert(extLocationHistory.length);
+};
 
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-      }
+function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(-34.397, 150.644),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      noClear: true,
+      zoomControl: false,
+      panControl: false,
+      rotateControl: false,
+      scaleControl: false,
+      disableDefaultUI: true
+
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
+    
+    var myFileReader = new dragAndDropFileReader(handleReaderLoad);
+  }
+
