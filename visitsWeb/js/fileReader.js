da5115563937f3abc3200db1552a3d7e061df119
Dominikus Baur <dominikus.baur@gmail.com>
2012-09-20 16:50:54 +0200

    added file handling stub [dropbox]

--- visitsWeb/js/fileReader.js
+++ visitsWeb/js/fileReader.js
@@ -27,4 +27,25 @@ function dragOver(evt) {
 function drop(evt) {
 	evt.stopPropagation();
 	evt.preventDefault();
+	
+	var files = evt.dataTransfer.files;
+	var count = files.length;
+	 
+	// Only call the handler if 1 or more files was dropped.
+	if (count > 0)
+		handleFiles(files);
+}
+
+function handleFiles(files){
+	var file = files[0];
+	 
+	document.getElementById("droplabel").innerHTML = "Processing " + file.name;
+	 
+	var reader = new FileReader();
+	 
+	// init the reader event handlers
+	reader.onload = handleReaderLoad;
+	 
+	// begin the read operation
+	reader.readAsDataURL(file);
 }
