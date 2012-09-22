f14b4b5e98c2803114a75fa4dc5fc0c53a3fee17
Dominikus Baur <dominikus.baur@gmail.com>
2012-09-22 21:22:39 +0200

    finished kml reader - still some mysterious bugs to go, though

--- visitsWeb/js/maps.js
+++ visitsWeb/js/maps.js
@@ -1,13 +1,74 @@
+var inputFileType = undefined;
+
+function readKmlLocations(kmlText){
+	var result = new Array();
+	
+	//find beginning and end of relevant substring
+	var startGxTrack = kmlText.indexOf("<gx:Track>");
+	var endGxTrack = kmlText.indexOf("</gx:Track>");
+	var relevantKmlText = kmlText.substring(startGxTrack, endGxTrack);
+	
+	var relevantKmlTextLines = relevantKmlText.split("\n");
+	var currentCoord = new GpsLoc(undefined,undefined,undefined);
+	for(var i = 0; i < relevantKmlTextLines.length; i++){
+		var currentLine = relevantKmlTextLines[i];
+		currentLine = jQuery.trim(currentLine);
+		if(currentLine.indexOf("<when>") != -1){
+			currentLine = currentLine.slice(6);	//remove front part ('<when>')
+			currentLine = currentLine.slice(0, currentLine.length - 7); //remove end part ('</when>')
+			currentWhen = new Date(currentLine);
+			console.log("converted when: " + currentLine + " to: " + currentWhen.toDateString() + " and: " + currentWhen.getTime());
+			currentCoord.timestamp = currentWhen.getTime() / 1000.0;		//convert to seconds
+			
+			//check if current point is finished
+			if(currentCoord.lon){
+				result.push(currentCoord);
+				currentCoord = new GpsLoc(undefined, undefined, undefined);
+			}
+		} else if(currentLine.indexOf("<gx:coord>") != -1){
+			currentLine = currentLine.slice(10);	//remove front part ('<gx:coord>')
+			currentLine = currentLine.slice(0, currentLine.length - 11); //remove end part ('</gx:coord>')
+			console.log("parsing coord: " + currentLine);
+			var splitCoordString = currentLine.split(" ");
+			//first value is lon, then lat, then altitude
+			currentCoord.lon = splitCoordString[0];
+			currentCoord.lat = splitCoordString[1];
+			
+			//check if current point is finished
+			if(currentCoord.timestamp){
+				result.push(currentCoord);
+				currentCoord = new GpsLoc(undefined, undefined, undefined);
+			}
+			
+		} else {
+			console.log("unknown type of line: " + currentLine);
+		}
+	}
+	return result;
+};
+
 var handleReaderLoad = function(evt){
-	var extLocationHistory = JSON.parse(evt.target.result);
 
 	var resultGpsLoc = new Array();
-	for(var i = 0; i < extLocationHistory.length; i++){
-		var extLocation = extLocationHistory[i];
-		var location = new GpsLoc(extLocation.lon, extLocation.lat, extLocation.t);
-		resultGpsLoc.push(location);
-	}
+
+	if(inputFileType == "json"){
+		var extLocationHistory = JSON.parse(evt.target.result);
 	
+		for(var i = 0; i < extLocationHistory.length; i++){
+			var extLocation = extLocationHistory[i];
+			var location = new GpsLoc(extLocation.lon, extLocation.lat, extLocation.t);
+			resultGpsLoc.push(location);
+		}
+	} else if(inputFileType == "kml"){
+		resultGpsLoc = readKmlLocations(evt.target.result);
+		
+	}
+
+	console.log("resulting array:");
+	for(var i = 0; i < resultGpsLoc.length; i++){
+		console.log("#" + i + ": lat: " + resultGpsLoc[i].lat + ", lon: " + resultGpsLoc[i].lon + ", timestamp: " + resultGpsLoc[i].timestamp);
+	}
+
 	var mainmodel = new Mainmodel(resultGpsLoc);
 	
 	var tlModel = new TimelineModel(mainmodel);
